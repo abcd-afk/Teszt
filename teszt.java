9c222e839c3d71e546285b79abfb5e1075d1acd8
unknown <takacsszilvia777@gmail.com>
2026-01-05 11:15:30 +0100

    Java

---
+++ teszt.java
@@ -0,0 +1,10 @@
+public class teszt {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        System.out.println("   |");
+        System.out.println("   |");
+    }
+}
