ec4eec77556aba6167eabd0eae97733f6037d6a6
Sbalint134 <122346232+abcd-afk@users.noreply.github.com>
2026-01-05 10:15:51 +0100

    Update teszt.py

--- teszt.py
+++ teszt.py
@@ -1,6 +1,6 @@
 for x in range(0 , 101):
     if x % 4 == 0 and x % 7 == 0:
-        print("fizz buzz")
+        print("fizz buzz ggggggg")
     elif x % 4 == 0:
         print("fizz")
     elif x % 7 ==0:
