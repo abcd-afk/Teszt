397da45898ee21483d02913f5815d0f18d39937c
unknown <takacsszilvia777@gmail.com>
2026-01-05 10:11:22 +0100

    Python

---
+++ teszt.py
@@ -0,0 +1,8 @@
+for x in range(0 , 101):
+    if x % 4 == 0 and x % 7 == 0:
+        print("fizz buzz")
+    elif x % 4 == 0:
+        print("fizz")
+    elif x % 7 ==0:
+        print("buzz")
+    
